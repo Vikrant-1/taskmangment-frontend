ca7ba7eed61c6bf21463f3b5dc41082df7a85caf
Vikrant-1 <vikrantkainth090699@gmail.com>
2024-09-07 15:43:30 +0530

    change input styles

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+
 interface TextInputProps {
   label?: string;
   value: string;
   placeholder: string;
   onChangeValue: (value: string) => void;
+  rightImage?: string;
+  leftImage?: string;
 }
 
 const TextInput = ({
@@ -10,7 +14,10 @@ const TextInput = ({
   value,
   onChangeValue,
   placeholder,
+  rightImage,
+  leftImage,
 }: TextInputProps) => {
+  const [isFocused, setIsFocused] = useState(false);
   return (
     <div>
       <label
@@ -19,14 +26,24 @@ const TextInput = ({
       >
         {label}
       </label>
-      <input
-        id="input"
-        type={label?.toLowerCase() === "password" ? "password" : "text"}
-        value={value}
-        onChange={(e) => onChangeValue(e.target.value)}
-        placeholder={placeholder}
-        className="block w-full rounded-md border-0 py-1.5 pl-3 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-      />
+      <div
+        className={`flex bg-white items-center border rounded-md ${
+          isFocused ? "border-indigo-600" : "border-gray-300"
+        } py-1.5 pl-3 pr-2`}
+      >
+        {leftImage && <img src={leftImage} className="w-5 h-5 mr-2" />}
+        <input
+          id="input"
+          type={label?.toLowerCase() === "password" ? "password" : "text"}
+          value={value}
+          onChange={(e) => onChangeValue(e.target.value)}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          placeholder={placeholder}
+          className="flex-grow outline-none bg-transparent text-gray-900 placeholder-gray-400 text-sm leading-6"
+        />
+        {rightImage && <img src={rightImage} className="w-5 h-5 mx-2" />}
+      </div>
     </div>
   );
 };
